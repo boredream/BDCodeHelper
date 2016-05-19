6d0564937a8f3040a0040cd7dd7d3251d1547f6f
lovegsh <loveghs@163.com>
2016-05-19 16:17:09 +0800

    滚轮控件

---
+++ src/main/java/com/boredream/bdcodehelper/entity/city/AddressModel.java
@@ -0,0 +1,11 @@
+package com.boredream.bdcodehelper.entity.city;
+
+import java.io.Serializable;
+
+public class AddressModel implements Serializable {
+
+    public String provinceName;
+    public String cityName;
+    public String districtName;
+
+}

25678deafb93083a66224ccb84541dea3c847cfd
lichunyang <lichunyang@qbao.com>
2017-03-01 14:46:53 +0800

    commit

--- src/main/java/com/boredream/bdcodehelper/BoreConstants.java
+++
@@ -1,9 +0,0 @@
-package com.boredream.bdcodehelper;
-
-public class BoreConstants {
-    private BoreConstants() {
-    }
-
-    public static boolean isUnitTest = false;
-
-}
--- src/main/java/com/boredream/bdcodehelper/net/MultiPageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.boredream.bdcodehelper.net;
-
-import rx.Observable;
-
-public abstract class MultiPageRequest<T> {
-
-    public abstract Observable<T> request(int page);
-
-}
